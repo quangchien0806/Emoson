389888ea8d3c8e27a70b62d9409992c348a1da04
Your Name <quangchien220399@gmail.com>
2022-04-25 08:13:07 +0700

    new coder

--- main.js
+++ main.js
@@ -3,5 +3,29 @@ const menu_mobi = document.querySelector(".menu-mobi");
 
 mobi_togglers.addEventListener("click", () => {
   menu_mobi.classList.toggle("active");
-  console.log("ôi");
+});
+
+const search = document.querySelector(".search");
+const input_search = document.querySelector(".input-search");
+search.addEventListener("click", () => {
+  input_search.classList.toggle("active");
+});
+
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 10,
+  responsive: {
+    500: {
+      items: 1,
+    },
+    700: {
+      items: 2,
+    },
+    1440: {
+      items: 3,
+    },
+    1920: {
+      items: 3,
+    },
+  },
 });
